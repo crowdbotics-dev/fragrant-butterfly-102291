1488c099e418a7f5ab0e26728b176aa82633f133
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 15:20:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-butterfly-102291",
+  url: "https://fragrant-butterfly-102291.botics.co",
+  api: "https://fragrant-butterfly-102291.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
